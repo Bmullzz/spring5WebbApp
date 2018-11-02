4c49dba52d770969b8acc40dc169cf2b0030bed8
Brian R Mullin <brmullin@email.msmary.edu>
2018-11-02 11:44:27 -0400

    @bmullin created Author and Book models

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,34 @@
+package guru.springframework.spring5webapp.model;
+
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    //private Set<Book> books
+
+    public Author() {
+
+    }
+
+    public Author(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
